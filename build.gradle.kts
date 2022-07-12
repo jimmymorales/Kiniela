3498b495c80f22d1c8b17bf4536fbe99f6e77c3c
Jimmy Morales <me@jimmymorales.dev>
2022-07-12 13:57:11 -0600

    Detekt Setup (#2)
    * chore: Setup detekt
    
    * chore: Configure Detekt for Compose
    
    * chore: Apply ktlint setup to IDEA config
    
    * chore: Apply detekt on files
    
    * chore: Setup editor config
    
    * docs: Static Analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,19 @@ plugins {
     id("com.android.application") version "7.4.0-alpha08" apply false
     id("com.android.library") version "7.4.0-alpha08" apply false
     kotlin("android") version "1.6.21" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.21.0-RC2"
+}
+
+subprojects {
+    apply {
+        plugin("io.gitlab.arturbosch.detekt")
+    }
+
+    detekt {
+        config = rootProject.files("config/detekt/detekt.yml")
+    }
+
+    dependencies {
+        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.21.0-RC2")
+    }
 }
