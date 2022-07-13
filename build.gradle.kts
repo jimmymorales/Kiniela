f69812954797631d770117dede3f4dd2c3dd1f58
Jimmy Morales <me@jimmymorales.dev>
2022-07-12 18:07:17 -0600

    chore: Add Kotlin plugin to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
 	id("com.android.application") version "7.4.0-alpha08" apply false
 	id("com.android.library") version "7.4.0-alpha08" apply false
-	kotlin("android") version "1.6.21" apply false
+	alias(libs.plugins.kgp) apply false
 	alias(libs.plugins.detekt)
 }
 
