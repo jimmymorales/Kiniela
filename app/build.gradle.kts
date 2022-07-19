188281e5a48937aabf07b87ff8cc4d70ce0d16df
Jimmy Morales <me@jimmymorales.dev>
2022-07-19 08:11:54 -0600

    chore: Target Java 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 plugins {
 	id("com.android.application")
 	kotlin("android")
@@ -27,11 +29,11 @@ android {
 		}
 	}
 	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_1_8
-		targetCompatibility = JavaVersion.VERSION_1_8
+		sourceCompatibility = JavaVersion.VERSION_11
+		targetCompatibility = JavaVersion.VERSION_11
 	}
 	kotlinOptions {
-		jvmTarget = "1.8"
+		jvmTarget = JavaVersion.VERSION_11.toString()
 		allWarningsAsErrors = true
 	}
 	buildFeatures {
